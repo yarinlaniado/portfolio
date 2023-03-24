3c68cb7dba4b6cb036e09c45d17bba283bf33ec3
Yarin <yarinl464@gmail.com>
2023-03-25 02:10:05 +0300

    Fixing bufferGeomatry issue

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,14 +1,14 @@
 import { useState, useRef, Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
-import * as random from "maath/random/dist/maath-random.esm";
+import * as random from "maath/random";
+import * as THREE from "three";
 
 const Stars = (props) => {
   const ref = useRef();
-  const [sphere] = useState(() =>
-    random.inSphere(new Float32Array(5000), { radius: 1.2 })
+  const [sphere, setSphere] = useState(() =>
+    random.inSphere(new Float32Array(5001), { radius: 1.2 })
   );
-
   useFrame((state, delta) => {
     ref.current.rotation.x -= delta / 10;
     ref.current.rotation.y -= delta / 15;
